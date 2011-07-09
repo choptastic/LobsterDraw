739afde8909404251f972308584b180fb0ad3f5a
Jesse Gumm <sigmastar@gmail.com>
2011-07-09 08:25:38 +0200

    canvas draws properly and sends it's change queue to the server

--- site/src/pages/play.erl
+++ site/src/pages/play.erl
@@ -12,7 +12,13 @@ title() ->
 	[].
 
 canvas() ->
-	"<canvas class=game_canvas width=640 height=480>Get a browser that doesn't suck</canvas>";
+	wf:wire(#api{name=queue,tag=unused}),
+	[
+	"<canvas class=game_canvas width=640 height=480>Get a browser that doesn't suck</canvas>",
+	#br{},
+	"<script>enable_drawing()</script>",
+	#button{text="Erase",postback=erase}
+	].
 	
 
 playerlist() ->
@@ -27,17 +33,13 @@ controls() ->
 	[].
 
 
-start_drawing() ->
-	
 
 event(guess) ->
 	GuessText = wf:q(guess);
+event(erase) ->
+	wf:wire("erase()");
 event(_) -> 
 	ok.
 
-api_event(api_draw,_,[line,Color,X1,Y1,X2,Y2]) ->
-	ok;
-api_event(api_draw,_,[erase]) ->
-	ok;
-api_event(api_draw,_,[fill,Color,X,Y]) ->
-	ok.
+api_event(queue,_,ActionList) ->
+	?PRINT(ActionList).
--- site/static/js/play.js
+++ site/static/js/play.js
@@ -3,42 +3,100 @@ var _width = 3;
 var _drawing = false;
 var _lastcoords = null;
 var _queue = [];
+var _queue_timer = null;
 
 function get_context()
 {
-	var canvas = $(".game_canvas");
+	var canvas = $(".game_canvas").get(0);
 	if(canvas.getContext) 
 		return canvas.getContext("2d");
 	else{
 		alert("Your browser doesn't support the canvas tag. Get one that doesn't suck.");
-		location.href="http://chrome.google.com";
+		//location.href="http://chrome.google.com";
 	}
 }
 
-function line(x1,y1,x2,y2)
+function draw_queue()
+{
+	for(i=0;i<_queue.length;i++)
+	{
+		var action = _queue[i];
+		switch(action[0])
+		{
+			case "line":
+				line(action[1],action[2],action[3],action[4],action[5],action[6]);
+				break;
+			case "fill":
+				fill(action[1],action[2],action[3]);
+				break;
+			case "erase":
+				erase();
+				break;
+		}
+	}
+	_queue = [];
+}
+
+
+function send_queue()
+{
+	var to_send = _queue;
+	_queue = [];
+
+	for(i=0;i<to_send.length;i++)
+	{
+		to_send[i][0] = Bert.atom(to_send[i][0]);
+	}
+	if(to_send.length>0)
+		page.queue(to_send);
+}
+		
+
+function line(color,width,x1,y1,x2,y2)
 {
 	var c = get_context();
+	c.beginPath();
 	c.moveTo(x1,y1);
-	c.strokeStyle = _color;
-	c.lineWidth = _width;
+	c.strokeStyle = color;
+	c.lineWidth = width;
 	c.lineTo(x2,y2);
 	c.stroke();
 	c.closePath();
 }
 
-function fill(x,y)
+function queue_line(x1,y1,x2,y2)
+{
+	line(_color,_width,x1,y1,x2,y2);
+	_queue.push(["line",_color,_width,x1,y1,x2,y2]);
+}
+
+function fill(color,x,y)
 {
 	var c = get_context();
 	c.moveTo(x,y);
-	c.fillStyle = _color;
+	c.fillStyle = color;
 	c.fill();
 	c.closePath();
+}
+
+function queue_fill(x,y)
+{
+	fill(_color,x,y);
+	_queue.push(["fill",_color,x,y]);
 }
 
 function erase()
 {
 	var c = get_context();
-	var c.clearRect(0,0,context.canvas.width,context.canvas.height);
+	c.save();
+	c.clearRect(0,0,c.canvas.width,c.canvas.height);
+	c.restore();
+}
+
+function queue_erase()
+{
+	erase();
+	_queue.push(["erase"]);
 }
 
 function get_coords(e)
@@ -62,11 +120,14 @@ function enable_drawing()
 		.mousemove(function(e){
 			if(_drawing) {
 				coords = get_coords(e);
-				line(	_lastcoords.x,
+				queue_line(	
+					_lastcoords.x,
 					_lastcoords.y,
 					coords.x,
 					coords.y);
 				_lastcoords = coords;
+
 			}
-		});		
+		});
+	_queue_timer = setInterval(send_queue,300);
 }
